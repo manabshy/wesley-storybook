330cd7b566829600164428a2a0ca2b7152c87ce6
Alex Cotelin <Alex.Cotelin@wesleyan.co.uk>
2020-06-05 08:50:05 +0100

    First question always sends attemptId null

--- libs/wpisa/feature-isa-journey/src/lib/core/knowledge-check.facade.ts
+++ libs/wpisa/feature-isa-journey/src/lib/core/knowledge-check.facade.ts
@@ -15,15 +15,17 @@ export class KnowledgeCheckFacade {
 
   submitQuestion(questionIndex: number, answer: string) {
     console.log('submitting', questionIndex, answer);
+    const attemptId = questionIndex === 1 ? null : this.knowledgeCheckAttemptId;
+
     return this.knowledgeCheckService
       .submitAnswer({
         questionText: 'test',
         questionIndex,
         answer,
-        attemptId: this.knowledgeCheckAttemptId,
+        attemptId,
       })
       .pipe(
-        tap(({ attemptId }) => (this.knowledgeCheckAttemptId = attemptId)),
+        tap((response) => (this.knowledgeCheckAttemptId = response.attemptId)),
         catchError((err) => {
           window.open(`${isaRoutesNames.KNOWLEDGE_CHECK_ERROR}`, '_self');
 
