db1742f91a14e4373f1447e31762cbb2e2edd9ad
Alex Cotelin <Alex.Cotelin@wesleyan.co.uk>
2020-06-20 22:10:10 +0100

    refactor placeholder replacement using ramda

--- libs/wpisa/feature-isa-journey/src/lib/core/investment-options.facade.ts
+++ libs/wpisa/feature-isa-journey/src/lib/core/investment-options.facade.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { formatCurrency } from '@angular/common';
 import { Observable } from 'rxjs';
 import { map, filter } from 'rxjs/operators';
-import { assocPath } from 'ramda';
+import { compose, assocPath, replace, over, lensPath } from 'ramda';
 
 import {
   ISAApiService,
@@ -28,97 +28,189 @@ export class InvestmentOptionsFacade {
   > = this.currentTaxPeriodISALimits$.pipe(
     filter((data) => !!data),
     map((tax) => {
-      const newOb = assocPath(
-        ['summary'],
-        this.pageContent.summary
-          .replace('{tax-year}', tax.taxPeriodDescription)
-          .replace(
-            '{total-annual-allowance}',
-            formatCurrencyGBP(tax.totalAnnualAllowance)
-          ),
-        this.pageContent
-      );
-
-      //   console.log('R', newOb);
-      return {
-        ...this.pageContent,
-        summary: this.pageContent.summary
-          .replace('{tax-year}', tax.taxPeriodDescription)
-          .replace(
-            '{total-annual-allowance}',
-            formatCurrencyGBP(tax.totalAnnualAllowance)
-          ),
-        singleLumpSum: {
-          ...this.pageContent.singleLumpSum,
-          summary: this.pageContent.singleLumpSum.summary.replace(
+      const placeholdersReplaced = compose(
+        over(
+          lensPath(['summary']),
+          compose(
+            replace('{tax-year}', tax.taxPeriodDescription),
+            replace(
+              '{total-annual-allowance}',
+              formatCurrencyGBP(tax.totalAnnualAllowance)
+            )
+          )
+        ),
+        over(
+          lensPath(['singleLumpSum', 'summary']),
+          replace(
             '{max-lump-sum-amount}',
             formatCurrencyGBP(tax.maxLumpSumAmount)
-          ),
-          lumpSumPayment: {
-            ...this.pageContent.singleLumpSum.lumpSumPayment,
-            summary: this.pageContent.singleLumpSum.lumpSumPayment.summary
-              .replace(
-                '{min-new-lump-sum-amount}',
-                formatCurrencyGBP(tax.minNewLumpSumAmount)
-              )
-              .replace(
-                '{max-lump-sum-amount}',
-                formatCurrencyGBP(tax.maxLumpSumAmount)
-              ),
-            lumpSumHelpText: this.pageContent.singleLumpSum.lumpSumPayment.lumpSumHelpText.replace(
+          )
+        ),
+        over(
+          lensPath(['singleLumpSum', 'lumpSumPayment', 'summary']),
+          compose(
+            replace(
               '{min-new-lump-sum-amount}',
               formatCurrencyGBP(tax.minNewLumpSumAmount)
             ),
-            lumpSumAmount: {
-              ...this.pageContent.singleLumpSum.lumpSumPayment.lumpSumAmount,
-              inputError: this.pageContent.singleLumpSum.lumpSumPayment.lumpSumAmount.inputError
-                .replace(
-                  '{min-new-lump-sum-amount}',
-                  formatCurrencyGBP(tax.minNewLumpSumAmount)
-                )
-                .replace(
-                  '{max-lump-sum-amount}',
-                  formatCurrencyGBP(tax.maxLumpSumAmount)
-                ),
-            },
-          },
-        },
-        monthlyPayments: {
-          ...this.pageContent.monthlyPayments,
-          monthlyPayment: {
-            ...this.pageContent.monthlyPayments.monthlyPayment,
-            monthlyHelpText: this.pageContent.monthlyPayments.monthlyPayment.monthlyHelpText.replace(
-              '{min-new-monthly-amount}',
-              formatCurrencyGBP(tax.minNewMonthlyAmount)
-            ),
-            monthlyAmount: {
-              ...this.pageContent.monthlyPayments.monthlyPayment.monthlyAmount,
-              inputError: this.pageContent.monthlyPayments.monthlyPayment.monthlyAmount.inputError.replace(
-                '{max-monthly-amount}',
-                formatCurrencyGBP(tax.maxMonthlyAmount)
-              ),
-              inputOtherError: this.pageContent.monthlyPayments.monthlyPayment.monthlyAmount.inputOtherError.replace(
-                '{min-new-monthly-amount}',
-                formatCurrencyGBP(tax.minNewMonthlyAmount)
-              ),
-            },
-          },
-        },
-        monthlyAndLumpSum: {
-          ...this.pageContent.monthlyAndLumpSum,
-          monthlyAndLumpSumPayment: {
-            ...this.pageContent.monthlyAndLumpSum.monthlyAndLumpSumPayment,
-            monthlyHelpText: this.pageContent.monthlyAndLumpSum.monthlyAndLumpSumPayment.monthlyHelpText.replace(
-              '{min-new-monthly-amount}',
-              formatCurrencyGBP(tax.minNewMonthlyAmount)
-            ),
-            lumpSumHelpText: this.pageContent.monthlyAndLumpSum.monthlyAndLumpSumPayment.lumpSumHelpText.replace(
+            replace(
+              '{max-lump-sum-amount}',
+              formatCurrencyGBP(tax.maxLumpSumAmount)
+            )
+          )
+        ),
+        over(
+          lensPath(['singleLumpSum', 'lumpSumPayment', 'lumpSumHelpText']),
+          compose(
+            replace(
+              '{min-new-lump-sum-amount}',
+              formatCurrencyGBP(tax.minNewLumpSumAmount)
+            )
+          )
+        ),
+        over(
+          lensPath([
+            'singleLumpSum',
+            'lumpSumPayment',
+            'lumpSumAmount',
+            'inputError',
+          ]),
+          compose(
+            replace(
               '{min-new-lump-sum-amount}',
               formatCurrencyGBP(tax.minNewLumpSumAmount)
             ),
-          },
-        },
-      };
+            replace(
+              '{max-lump-sum-amount}',
+              formatCurrencyGBP(tax.maxLumpSumAmount)
+            )
+          )
+        ),
+        over(
+          lensPath(['monthlyPayments', 'monthlyPayment', 'monthlyHelpText']),
+          compose(
+            replace(
+              '{min-new-monthly-amount}',
+              formatCurrencyGBP(tax.minNewMonthlyAmount)
+            )
+          )
+        ),
+        over(
+          lensPath([
+            'monthlyPayments',
+            'monthlyPayment',
+            'monthlyAmount',
+            'inputError',
+          ]),
+          compose(
+            replace(
+              '{max-monthly-amount}',
+              formatCurrencyGBP(tax.maxMonthlyAmount)
+            )
+          )
+        ),
+        over(
+          lensPath([
+            'monthlyPayments',
+            'monthlyPayment',
+            'monthlyAmount',
+            'inputOtherError',
+          ]),
+          compose(
+            replace(
+              '{min-new-monthly-amount}',
+              formatCurrencyGBP(tax.minNewMonthlyAmount)
+            )
+          )
+        ),
+        over(
+          lensPath([
+            'monthlyAndLumpSum',
+            'monthlyAndLumpSumPayment',
+            'monthlyHelpText',
+          ]),
+          compose(
+            replace(
+              '{min-new-monthly-amount}',
+              formatCurrencyGBP(tax.minNewMonthlyAmount)
+            )
+          )
+        ),
+        over(
+          lensPath([
+            'monthlyAndLumpSum',
+            'monthlyAndLumpSumPayment',
+            'lumpSumHelpText',
+          ]),
+          compose(
+            replace(
+              '{min-new-lump-sum-amount}',
+              formatCurrencyGBP(tax.minNewLumpSumAmount)
+            )
+          )
+        ),
+        over(
+          lensPath([
+            'monthlyAndLumpSum',
+            'monthlyAndLumpSumPayment',
+            'summary',
+          ]),
+          compose(
+            replace(
+              '{total-annual-allowance}',
+              formatCurrencyGBP(tax.totalAnnualAllowance)
+            )
+          )
+        ),
+        over(
+          lensPath([
+            'monthlyAndLumpSum',
+            'monthlyAndLumpSumPayment',
+            'lumpSumAmount',
+            'inputError',
+          ]),
+          compose(
+            replace(
+              '{min-new-lump-sum-amount}',
+              formatCurrencyGBP(tax.minNewLumpSumAmount)
+            ),
+            replace(
+              '{max-lump-sum-amount}',
+              formatCurrencyGBP(tax.maxLumpSumAmount)
+            )
+          )
+        ),
+        over(
+          lensPath([
+            'monthlyAndLumpSum',
+            'monthlyAndLumpSumPayment',
+            'monthlyAmount',
+            'inputError',
+          ]),
+          compose(
+            replace(
+              '{max-monthly-amount}',
+              formatCurrencyGBP(tax.maxMonthlyAmount)
+            )
+          )
+        ),
+        over(
+          lensPath([
+            'monthlyAndLumpSum',
+            'monthlyAndLumpSumPayment',
+            'monthlyAmount',
+            'inputOtherError',
+          ]),
+          compose(
+            replace(
+              '{min-new-monthly-amount}',
+              formatCurrencyGBP(tax.minNewMonthlyAmount)
+            )
+          )
+        )
+      )(this.pageContent);
+
+      return placeholdersReplaced;
     })
   );
 
