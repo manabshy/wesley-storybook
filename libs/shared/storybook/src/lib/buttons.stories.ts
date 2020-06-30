d4a961aa7fb39f149c024ce87fdacf021738cde8
Alex Cotelin <Alex.Cotelin@wesleyan.co.uk>
2020-06-30 22:18:45 +0100

    underline, slate animation

--- libs/shared/storybook/src/lib/buttons.stories.ts
+++ libs/shared/storybook/src/lib/buttons.stories.ts
@@ -18,9 +18,10 @@ export default {
 export const buttons = () => ({
   template: `
   <div class="bg-solid-white" style="padding:2rem;">
-  <p><button class="wes-button wes-button__primary">Continue</button></p>
-  <p><button class="wes-button wes-button__primary wes-button--disabled" disabled>Continue</button></p>
-  <p><button class="wes-button wes-button__link" >Enter address manually</button></p>
+  <p><button class="wes-button wes-button-primary">Continue</button></p>
+  <p><button class="wes-button wes-button-primary wes-button--disabled" disabled>Continue</button></p>
+  <p class="wes-bg-solid-gold-900 " style="padding: 2rem"><button class="wes-button wes-button-cta">Open your ISA now</button></p>
+  <p><button class="wes-button wes-button-link" >Enter address manually</button></p>
   <p> Wesleyan Unit Trust Managers Ltd and Wesleyan Financial Services are wholly owned by Wesleyan Assurance Society which is authorised by the Prudential Regulation Authority and regulated by the Financial Conduct Authority and the Prudential Regulation Authority. <a href="#" class="wes-link">Click for more information</a> about the Wesleyan group of companies.© 2020 Wesleyan Assurance Society. All rights reserved, for UK use only.
   </p>
 
