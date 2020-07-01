5d668dfc435668df591b08cd88aeb26e1b973879
Alex Cotelin <Alex.Cotelin@wesleyan.co.uk>
2020-07-01 08:37:26 +0100

    p and ul

--- libs/shared/storybook/src/lib/typography.stories.ts
+++ libs/shared/storybook/src/lib/typography.stories.ts
@@ -18,6 +18,7 @@ export default {
 export const headings = () => ({
   template: `
   <div class="bg-solid-white" style="padding:2rem;">
+  <h1 class="wes-text-mega">Mega About our Financial Consultants</h1>
   <h1>H1 About our Financial Consultants</h1>
   <h2>H2 About our Financial Consultants</h2>
   <h3>H3 About our Financial Consultants</h3>
@@ -29,6 +30,32 @@ export const headings = () => ({
 
   `,
 });
-export const gradient = () => ({
-  template: `<div class="bg-gradient-yellow" style="padding:3rem">.bg-gradient-yellow</div>`,
+export const paragraphs = () => ({
+  template: `
+    <div class="" style="padding:3rem">
+
+  <p class="wes-text-intro">Intro sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque.</p>
+  <p class="wes-text-intro">Intro sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque.</p>
+
+  <p class="wes-text-longform">Longform sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem.</p>
+  <p class="wes-text-longform">Longform sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem.</p>
+
+  <p class="wes-text-shortform">Shortform sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem.</p>
+  <p class="wes-text-shortform">Shortform sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem.</p>
+
+  <p class="wes-text-xs">Shortform sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem.</p>
+  <p class="wes-text-xs">Shortform sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem.</p>
+    </div>`,
+});
+export const lists = () => ({
+  template: `
+    <div class="" style="padding:3rem">
+    <ul class="wes-list">
+    <li class="wes-list-bullets">Tailored, one-to-one advice from our Financial Consultants available at your home or place of work</li>
+    <li class="wes-list-bullets">First appointments typically last an hour and will help us understand your current financial situation and long-term goals</li>
+    <li class="wes-list-bullets">After the appointment your Financial Consultant will create a plan of action to help you achieve your goals</li>
+    <li class="wes-list-bullets">Your Financial Consultant will then guide you through the process of purchasing and monitoring the performance of these products</li>
+  </ul>
+
+    </div>`,
 });
