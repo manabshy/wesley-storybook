c64288aaab96e559bd923c2acf73c24c4588d2b0
Alex Cotelin <Alex.Cotelin@wesleyan.co.uk>
2020-07-16 10:55:39 +0100

    announce session countdown every 10sec

--- libs/wpisa/feature-inactivity-timeout/src/lib/inactivity-modal/inactivity-modal.component.ts
+++ libs/wpisa/feature-inactivity-timeout/src/lib/inactivity-modal/inactivity-modal.component.ts
@@ -7,20 +7,30 @@ import {
 } from '@wesleyan-frontend/wpisa/data-access';
 
 import { InactivityModalData } from './inactivity-modal-data.interface';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { Observable } from 'rxjs';
+import { tap, finalize, every, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'wes-inactivity-modal',
   templateUrl: './inactivity-modal.component.html',
   styleUrls: ['./inactivity-modal.component.scss'],
 })
-export class InactivityModalComponent implements OnInit {
+export class InactivityModalComponent {
   content: InactivityModal;
+  countDownWithAlert$: Observable<number> = this.data.countDown.pipe(
+    tap((seconds) =>
+      seconds % 10 === 0
+        ? this.liveAnnouncer.announce(`Session expires in ${seconds} seconds`)
+        : ''
+    ),
+    finalize(() => this.liveAnnouncer.clear())
+  );
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: InactivityModalData,
-    private configService: ConfigService
+    private configService: ConfigService,
+    private liveAnnouncer: LiveAnnouncer
   ) {
     this.content = this.configService.content.inactivityModal;
   }
-
-  ngOnInit(): void {}
 }
