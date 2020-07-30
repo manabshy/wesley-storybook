992691eed37cd3cce5b2cd57ee9df77fbab17f4f
Alex Cotelin <Alex.Cotelin@wesleyan.co.uk>
2020-07-30 15:01:59 +0100

    #83833 show fraction .50

--- libs/wpisa/feature-isa-journey/src/lib/core/util-functions.ts
+++ libs/wpisa/feature-isa-journey/src/lib/core/util-functions.ts
@@ -1,5 +1,8 @@
 import { formatCurrency } from '@angular/common';
 
 export function formatCurrencyGBP(value: number) {
-  return formatCurrency(value, 'en-gb', '£', 'GBP', '1.0-2');
+  //Show 100.50 not 100.5 and 100 not 100.00
+  const fraction = value % 10 === 0 ? '1.0-2' : '1.2-2';
+
+  return formatCurrency(value, 'en-gb', '£', 'GBP', fraction);
 }
