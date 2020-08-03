ab4018317db9b989bd24f8c4a5ddde665299c7db
Ricky Dawson <ricky.dawson@wesleyan.co.uk>
2020-08-03 12:53:01 +0100

    change tax year to xxxx/yyyy

--- libs/wpisa/feature-isa-journey/src/lib/core/services/investment-options.facade.ts
+++ libs/wpisa/feature-isa-journey/src/lib/core/services/investment-options.facade.ts
@@ -54,7 +54,7 @@ export class InvestmentOptionsFacade {
           '{tax-year}',
           format(new Date(tax.startDateTime), 'yyyy') +
             '/' +
-            format(new Date(tax.endDateTime), 'yy')
+            format(new Date(tax.endDateTime), 'yyyy')
         ),
         replace(
           '{total-annual-allowance}',
