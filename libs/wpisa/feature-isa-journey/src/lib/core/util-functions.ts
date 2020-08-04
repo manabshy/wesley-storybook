df6f1bfd25ea9bcff2061b9db1321700a629d62e
Alex Cotelin <Alex.Cotelin@wesleyan.co.uk>
2020-08-04 14:56:35 +0100

    fix currency formatting

--- libs/wpisa/feature-isa-journey/src/lib/core/util-functions.ts
+++ libs/wpisa/feature-isa-journey/src/lib/core/util-functions.ts
@@ -2,7 +2,7 @@ import { formatCurrency } from '@angular/common';
 
 export function formatCurrencyGBP(value: number) {
   //Show 100.50 not 100.5 and 100 not 100.00
-  const fraction = value % 10 === 0 ? '1.0-2' : '1.2-2';
+  const fraction = value % 1 === 0 ? '1.0-2' : '1.2-2';
 
   return formatCurrency(value, 'en-gb', '£', 'GBP', fraction);
 }
