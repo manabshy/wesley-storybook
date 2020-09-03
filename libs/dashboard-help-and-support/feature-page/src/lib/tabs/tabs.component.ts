bc77504e0a0fa82741766ac2a119cec10dad10c3
Khuram.Shahzad <khuram.shahzad@wesleyan.co.uk>
2020-09-03 17:05:19 +0100

    Updated html and css

--- libs/dashboard-help-and-support/feature-page/src/lib/tabs/tabs.component.ts
+++ libs/dashboard-help-and-support/feature-page/src/lib/tabs/tabs.component.ts
@@ -1,18 +1,34 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ConfigService, Config } from '@wesleyan-frontend/dashboard-help-and-support/data-access';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { ConfigService, Config , Article } from '@wesleyan-frontend/dashboard-help-and-support/data-access';
 
 @Component({
   selector: 'wes-tabs',
   templateUrl: './tabs.component.html',
-  styleUrls: ['./tabs.component.scss']
+  styleUrls: ['./tabs.component.scss'],
+  encapsulation : ViewEncapsulation.None
 })
 export class TabsComponent implements OnInit {
   @Input() config: Config;
-  constructor() { }
+  articles: Article[];
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
+    const groupsDict = this.config.data.sections.reduce((acc, cur) => {
+      cur.articles.forEach(({ title}) => {
+        acc[title] = acc[title] || { title };
+        acc[title].items = acc[title].items || [];
+        acc[title].items.push({ id: cur.dataGtmId, title: cur.title });
+      });
+      return acc;
+    }, {});
+
+    this.articles = Object.keys(groupsDict).map(x => groupsDict[x]);
   }
   transform(value: string): string {
     return value? value.split(' ').join('_').replace('?','') : value;
   } 
+
+  
 }
