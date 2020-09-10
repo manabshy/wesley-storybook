aa4acf491b6ba2b59cb3b17893c95539c9fbed80
Khuram.Shahzad <khuram.shahzad@wesleyan.co.uk>
2020-09-10 09:44:36 +0100

    invest calculator html/css

--- libs/just-invest-calculator/feature-page/src/lib/invest-calculator/invest-calculator.component.ts
+++ libs/just-invest-calculator/feature-page/src/lib/invest-calculator/invest-calculator.component.ts
@@ -6,12 +6,20 @@ import { ConfigService, Config } from '@wesleyan-frontend/just-invest-calculator
   templateUrl: './invest-calculator.component.html',
   styleUrls: ['./invest-calculator.component.scss']
 })
+
 export class InvestCalculatorComponent implements OnInit {
  config : Config
-  constructor(private configService:ConfigService){
-    this.config =this.configService.content;
-  }
+ inputPerMonth = 0;
+ inputLumpsum = 0;
+ inputTerm = 0;
 
+  constructor(private configService:ConfigService){
+    this.config = this.configService.content;
+    this.inputTerm = this.config.calculator.budget.sliders[2].value;
+  }
+  onInvestPerMonthChange(event:any){ this.inputPerMonth = event.target.value;}
+  onLumpSumChange(event:any){ this.inputLumpsum = event.target.value;}
+  onTermChange(event:any){ this.inputTerm = event.target.value;}
   ngOnInit(): void {
   }
 
