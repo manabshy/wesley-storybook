2eaec8aa4dc2e8ba35f2b068ef88c402f9f49f6d
andy.perry@wesleyan.co.uk <andy.perry@wesleyan.co.uk>
2020-09-17 11:32:56 +0100

    feat(cta-card): initial implementation of cta-card for campaign pages

---
+++ libs/shared/storybook/src/lib/components/cta-card/cta-card.stories.ts
@@ -0,0 +1,39 @@
+export default {
+  title: 'Components|CTA Card',
+};
+
+export const defaultCtaCard = () => ({
+  template: `
+    <div class="bg--grey">
+      <div class="container">
+        <div class="col-md-12" style="padding:20px 0">
+
+          <!-- Component root -->
+          <div class="wes-cta-card">
+            <div class="wes-cta-card__body">
+              <div class="wes-cta-card__icon">
+                <svg width="45" height="56" xmlns="http://www.w3.org/2000/svg">
+                  <g fill="none" fill-rule="evenodd">
+                    <path stroke="#FEBD11" stroke-linecap="square" d="M1 46.5h42.5"/>
+                    <path d="M25.5.5v22.557L18 17.969l-7.5 5.088V.5h15z" stroke="#FEBD11"/>
+                    <path d="M.5.5h42a2 2 0 012 2v51a2 2 0 01-2 2H.5h0V.5z" stroke="#1E2630" stroke-linecap="round"/>
+                    <path stroke="#1E2630" stroke-linecap="square" d="M35.5.5v54"/>
+                  </g>
+                </svg>
+              </div>
+              <div class="wes-cta-card__content">
+                <div class="wes-cta-card__message">
+                  <p>Book today and we'll also send you our free guide to the NHS pension - packed with useful information to help you make sense of the scheme.</p>
+                </div>
+                <div class="wes-cta-card__action">
+                  <a class="wes-link" href="#">Get your free NHS pension guide</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  `,
+});
