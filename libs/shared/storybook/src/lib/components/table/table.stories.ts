68ac09b0cd371dd8bc9822ece885d581a5989e5d
Andy Perry <andy.perry@wesleyan.co.uk>
2020-10-01 16:28:12 +0100

    feat(table): third Table pattern variant for Campaigns

---
+++ libs/shared/storybook/src/lib/components/table/table.stories.ts
@@ -0,0 +1,119 @@
+export default {
+  title: 'Components|Table',
+};
+
+export const style1 = () => ({
+  template: `
+    <div class="container">
+      <div class="col-md-6 offset-md-3" style="padding:20px 0">
+
+        <div class="wes-table-style-1">
+          <table>
+            <tr>
+              <td>Save up to £20,000 in the 2020/2021 tax year</td>
+            </tr>
+            <tr>
+              <td>Pay no income tax or capital gains tax on your investment returns</td>
+            </tr>
+            <tr>
+              <td>Share in Wesleyan's profits through the With Profits ISA Fund</td>
+            </tr>
+          </table>
+        </div>
+
+      </div>
+    </div>
+  `,
+});
+
+export const style2 = () => ({
+  template: `
+    <div class="container">
+      <div class="col-md-6 offset-md-3" style="padding:20px 0">
+
+        <div class="wes-table-style-2">
+          <table>
+            <tr>
+              <td>Save up to £20,000 in the 2020/2021 tax year</td>
+            </tr>
+            <tr>
+              <td>Pay no income tax or capital gains tax on your investment returns</td>
+            </tr>
+            <tr>
+              <td>Share in Wesleyan's profits through the With Profits ISA Fund</td>
+            </tr>
+          </table>
+        </div>
+
+      </div>
+    </div>
+  `,
+});
+
+export const style3 = () => ({
+  template: `
+    <div class="container">
+      <div class="col-md-10 offset-md-1" style="padding:20px 0">
+
+        <div class="wes-table-style-3">
+          <table>
+            <tr>
+              <th>Worldwide all risks cover with full accidental damage</th>
+              <td>Take your possessions overseas when you travel, safe in the knowledge that they are automatically covered for loss, damage or theft.</td>
+              <td class="tick">
+                <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <g  stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+                        <g id="1_ISA-Product-Page_1366" transform="translate(-193.000000, -830.000000)" stroke="#FEBD11" stroke-width="2">
+                            <g id="Key-Features" transform="translate(0.000000, 584.000000)">
+                                <g id="List_Col1" transform="translate(193.000000, 244.000000)">
+                                    <g id="Icon_Tick" transform="translate(0.000000, 2.000000)">
+                                        <g id="Group-6" transform="translate(3.000000, 6.000000)">
+                                            <polyline id="Path-2" points="0 6.48317308 5.98043191 12 18 0"></polyline>
+                                        </g>
+                                    </g>
+                                </g>
+                            </g>
+                        </g>
+                    </g>
+                </svg>
+              </td>
+            </tr>
+            <tr>
+              <th>Automatic cover on new purchases</th>
+              <td>Treating yourself to something new? It’ll be covered automatically for up to 60 days before you need to tell us (subject to a maximum value, dependent on insurer).</td>
+              <td class="tick">
+                <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <g  stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+                        <g id="1_ISA-Product-Page_1366" transform="translate(-193.000000, -830.000000)" stroke="#FEBD11" stroke-width="2">
+                            <g id="Key-Features" transform="translate(0.000000, 584.000000)">
+                                <g id="List_Col1" transform="translate(193.000000, 244.000000)">
+                                    <g id="Icon_Tick" transform="translate(0.000000, 2.000000)">
+                                        <g id="Group-6" transform="translate(3.000000, 6.000000)">
+                                            <polyline id="Path-2" points="0 6.48317308 5.98043191 12 18 0"></polyline>
+                                        </g>
+                                    </g>
+                                </g>
+                            </g>
+                        </g>
+                    </g>
+                </svg>
+              </td>
+            </tr>
+            <tr>
+              <th>Cover for collections</th>
+              <td>Whatever you like to collect – be it art, antiques, clothes or jewellery – high-value home insurance will protect the possessions you value most.</td>
+              <td class="cross">
+                <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg">
+                  <g stroke="#FEBD11" stroke-width="2" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M11 1L1 11M1 1l10 10L1 1z"/>
+                  </g>
+                </svg>
+              </td>
+            </tr>
+          </table>
+        </div>
+
+      </div>
+    </div>
+  `,
+});
