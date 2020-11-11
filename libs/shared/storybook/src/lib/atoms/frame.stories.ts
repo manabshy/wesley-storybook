819e91b15dd08469c853f66ccdf2ed85782f88c1
Andy Perry <andy.perry@wesleyan.co.uk>
2020-11-11 15:24:46 +0000

    Additional frame class. Added validation styling for form controls.

--- libs/shared/storybook/src/lib/atoms/frame.stories.ts
+++ libs/shared/storybook/src/lib/atoms/frame.stories.ts
@@ -2,6 +2,14 @@ export default {
   title: 'Atoms|Frame',
 };
 
+export const square = () => ({
+  template: `
+    <div class="wes-square-frame">
+      <p><br><br></p>
+    </div>
+  `,
+});
+
 export const goldAccentTop = () => ({
   template: `
     <div class="wes-rounded-frame-with-accent-top">
