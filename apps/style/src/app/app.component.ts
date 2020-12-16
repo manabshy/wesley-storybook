6083c9cfa47e3401d7908daf163ca98e7e521623
Alex Cotelin <Alex.Cotelin@wesleyan.co.uk>
2020-12-16 15:32:24 +0000

    update component html

--- apps/style/src/app/app.component.ts
+++ apps/style/src/app/app.component.ts
@@ -4,6 +4,4 @@ import { Component } from '@angular/core';
   selector: 'wesleyan-frontend-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  title = 'style';
-}
+export class AppComponent {}
