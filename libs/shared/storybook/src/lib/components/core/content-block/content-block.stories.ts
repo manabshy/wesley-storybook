5ec4e9feed65eccc7f218daefd4f7cd0d5bd2af6
Andy Perry <andy.perry@wesleyan.co.uk>
2020-12-29 19:41:43 +0000

    RTE spacing implementation.

--- libs/shared/storybook/src/lib/components/core/content-block/content-block.stories.ts
+++ libs/shared/storybook/src/lib/components/core/content-block/content-block.stories.ts
@@ -29,8 +29,16 @@ export const contentBlock = () => ({
 
               <!-- Rich Text start -->
               <div class="component rich-text wes-rich-text-scope">
-                <h2><span class="h2">NHS pensions advice, tailored to you</span></h2>
+                <h1 class="level1">L1 - Unit trusts</h1>
+                <p class="lead">Lead - Wesleyan Unit Trust Managers (WUTM) offers a trio of products that enable you to invest in fully managed unit trust funds, run by an award-winning team. Whether you’re looking to build your own investment or save for your children’s future, we’ve got a product for you.</p>
+                <h2 class="level2">L2 - What is a stocks and shares ISA?</h2>
                 <p>Limits, exclusions and charges do apply. Full terms and conditions of the policy and cover, including the policy benefits and exclusions, will be contained in the Policy Wording and Policy Summary.</p>
+                <p>Limits, exclusions and charges do apply. Full terms and conditions of the policy and cover, including the policy benefits and exclusions, will be contained in the Policy Wording and Policy Summary.</p>
+                <h3 class="level3">L3 - The tax benefits of an investment ISA</h3>
+                <p>Limits, exclusions and charges do apply. Full terms and conditions of the policy and cover, including the policy benefits and exclusions, will be contained in the Policy Wording and Policy Summary.</p>
+                <h4 class="level4">L4 - How long does the meeting last?</h4>
+                <p>Limits, exclusions and charges do apply. Full terms and conditions of the policy and cover, including the policy benefits and exclusions, will be contained in the Policy Wording and Policy Summary.</p>
+                
                 <ul class="ticked">
                   <li>How can I retire early?</li>
                   <li>How do I maximise my Pension Lifetime Allowance?</li>
@@ -40,16 +48,17 @@ export const contentBlock = () => ({
                 </ul>
                 <p>Limits, exclusions and charges do apply. Full <a href="#">terms and conditions</a> of the policy and cover, including the policy benefits and exclusions, will be contained in the Policy Wording and Policy Summary.</p>
                 
-                <h3><span class="h3">Highlight effect</span></h3>
-                <h4>Left</h4>
-                <p class="highlight">Limits, exclusions and charges do apply. Full <a href="#">terms and conditions</a> of the policy and cover, including the policy benefits and exclusions, will be contained in the Policy Wording and Policy Summary.</p>
-                
-                <h4>Right</h4>
-                <p class="highlight" style="text-align: right">Limits, exclusions and charges do apply. Full <a href="#">terms and conditions</a> of the policy and cover, including the policy benefits and exclusions, will be contained in the Policy Wording and Policy Summary.</p>
-                
-                <h3><span class="h3">Emphasis effect</span></h3>
+                <h3 class="level3">L3 - Emphasis effect</h3>
                 <p class="emphasis">I walked down the Kokusai Dori, the main street of the city, doubling back and weaving off to lose anybody who was trailing me. Unfortunately my alpha potential is still too weak to achieve invisibility, so I have to shake trailers the old-fashioned way.</p>
                 
+                <h3 class="level2">L2 - Highlight effect</h3>
+                <h4 class="level3">L3 - Left</h4>
+                <p class="highlight">Limits, exclusions and charges do apply. Full <a href="#">terms and conditions</a> of the policy and cover, including the policy benefits and exclusions, will be contained in the Policy Wording and Policy Summary.</p>
+                
+                <h4 class="level3">L3 - Right</h4>
+                <p class="highlight" style="text-align: right">Limits, exclusions and charges do apply. Full <a href="#">terms and conditions</a> of the policy and cover, including the policy benefits and exclusions, will be contained in the Policy Wording and Policy Summary.</p>
+
+                <h5 class="level4">L4 - Lorem ipsum</h5>
                 <p>Limits, exclusions and charges do apply. Full terms and conditions of the policy and cover, including the policy benefits and exclusions, will be contained in the Policy Wording and Policy Summary.</p>
                 <ul class="bulleted">
                   <li>How can I retire early?</li>
