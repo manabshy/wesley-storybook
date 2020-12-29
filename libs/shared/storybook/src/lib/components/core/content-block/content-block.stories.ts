0a486464b7eba8c8a2b9f385980cecfa9b4c8acf
Andy Perry <andy.perry@wesleyan.co.uk>
2020-12-29 13:11:38 +0000

    Content Block is collapsible.

--- libs/shared/storybook/src/lib/components/core/content-block/content-block.stories.ts
+++ libs/shared/storybook/src/lib/components/core/content-block/content-block.stories.ts
@@ -6,9 +6,13 @@ export default {
 
 const variants = {
   'On White': '',
-  'On Grey': 'wes-content-block--grey',
+  'On White :: Collapsed': 'wes-content-block--collapse',
   'On White :: Right Aligned': 'wes-content-block--right',
+  'On White :: Right Aligned :: Collapsed': 'wes-content-block--right wes-content-block--collapse',
+  'On Grey': 'wes-content-block--grey',
+  'On Grey :: Collapsed': 'wes-content-block--grey wes-content-block--collapse',
   'On Grey :: Right Aligned': 'wes-content-block--right wes-content-block--grey',
+  'On Grey :: Right Aligned :: Collapsed': 'wes-content-block--right wes-content-block--grey wes-content-block--collapse',
   'On White :: Campaign': 'wes-content-block--campaign',
   'On Grey :: Campaign': 'wes-content-block--campaign wes-content-block--grey',
 };
