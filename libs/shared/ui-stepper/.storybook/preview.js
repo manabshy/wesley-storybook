b5ef4ada5b14f4aa74b27ff316117715402fafd6
MJ B <mj@MJs-MacBook-Pro.local>
2021-01-14 11:58:25 +0000

    Add storybook addon docs and notes

--- libs/shared/ui-stepper/.storybook/preview.js
+++ libs/shared/ui-stepper/.storybook/preview.js
@@ -1,6 +1,9 @@
 import { configure, addDecorator } from '@storybook/angular';
 import { withKnobs } from '@storybook/addon-knobs';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { DocsContainer, DocsPage } from '@storybook/addon-docs';
+import { setCompodocJson} from '@storybook/addon-docs/dist/frameworks/angular';
+import docJson from '../../../../documentation.json';
 
 addDecorator(withKnobs);
 configure(require.context('../src/lib', true, /\.stories\.(j|t)sx?$/), module);
@@ -8,4 +11,11 @@ export const parameters = {
   viewport: {
     viewports: INITIAL_VIEWPORTS,
   },
+  docs: {
+    container: DocsContainer,
+    page: DocsPage
+  }
 };
+setCompodocJson(docJson);
+
+
