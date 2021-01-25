af67b6a11393d9ef8438acd837209a43a5da1b90
Femi Ojemuyiwa <ojemuyiwa@gmail.com>
2021-01-25 18:12:41 +0000

    fix: all table variants rendered

--- libs/shared/storybook/src/lib/components/table/table.stories.ts
+++ libs/shared/storybook/src/lib/components/table/table.stories.ts
@@ -1,3 +1,7 @@
+import { addParameters } from '@storybook/angular';
+
+addParameters({ docs: { iframeHeight: 500 } });
+
 export default {
   title: 'Components/Table',
 };
@@ -70,109 +74,214 @@ export const dark_grey = () => ({
   `,
 });
 
-export const style3 = () => ({
+export const transparent = () => ({
   template: `
-    <div class="container">
-      <div class="col-md-10 offset-md-1" style="padding:20px 0">
-
-        <div class="wes-table-style-3">
-          <table>
-            <tr>
-              <th>Worldwide all risks cover with full accidental damage</th>
-              <td>Take your possessions overseas when you travel, safe in the knowledge that they are automatically covered for loss, damage or theft.</td>
-              <td class="tick">
-                <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
-                        <g id="1_ISA-Product-Page_1366" transform="translate(-193.000000, -830.000000)" stroke="#FEBD11" stroke-width="2">
-                            <g id="Key-Features" transform="translate(0.000000, 584.000000)">
-                                <g id="List_Col1" transform="translate(193.000000, 244.000000)">
-                                    <g id="Icon_Tick" transform="translate(0.000000, 2.000000)">
-                                        <g id="Group-6" transform="translate(3.000000, 6.000000)">
-                                            <polyline id="Path-2" points="0 6.48317308 5.98043191 12 18 0"></polyline>
-                                        </g>
-                                    </g>
-                                </g>
-                            </g>
-                        </g>
-                    </g>
-                </svg>
-              </td>
-            </tr>
-            <tr>
-              <th>Automatic cover on new purchases</th>
-              <td>Treating yourself to something new? It’ll be covered automatically for up to 60 days before you need to tell us (subject to a maximum value, dependent on insurer).</td>
-              <td class="tick">
-                <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
-                        <g id="1_ISA-Product-Page_1366" transform="translate(-193.000000, -830.000000)" stroke="#FEBD11" stroke-width="2">
-                            <g id="Key-Features" transform="translate(0.000000, 584.000000)">
-                                <g id="List_Col1" transform="translate(193.000000, 244.000000)">
-                                    <g id="Icon_Tick" transform="translate(0.000000, 2.000000)">
-                                        <g id="Group-6" transform="translate(3.000000, 6.000000)">
-                                            <polyline id="Path-2" points="0 6.48317308 5.98043191 12 18 0"></polyline>
-                                        </g>
-                                    </g>
-                                </g>
-                            </g>
-                        </g>
-                    </g>
-                </svg>
-              </td>
-            </tr>
-            <tr>
-              <th>Cover for collections</th>
-              <td>Whatever you like to collect – be it art, antiques, clothes or jewellery – high-value home insurance will protect the possessions you value most.</td>
-              <td class="cross">
-                <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg">
-                  <g stroke="#FEBD11" stroke-width="2" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
-                    <path d="M11 1L1 11M1 1l10 10L1 1z"/>
-                  </g>
-                </svg>
-              </td>
-            </tr>
-          </table>
-        </div>
-
-      </div>
+    <div class="container wes-table-container">
+      <table class="wes-table transparent">
+      <caption>England and Wales NHS pension contributions</caption>
+      <thead>
+        <tr>
+          <th colspan="2"></th>
+          <th>Annuities</th>
+          <th>Cash lump sums</th>
+          <th>Drawdown</th>
+          <th>Taking your whole pot as cash</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th colspan="2">
+            Provides an income?
+          </th>
+          <td data-title="Annuities">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Cash lump sums">
+            <span aria-label="cross" class="icon-cross"></span>
+          </td>
+          <td data-title="Drawdown">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Taking your whole pot as cash">
+            <span aria-label="cross" class="icon-cross"></span>
+          </td>
+        </tr>
+        <tr>
+          <th colspan="2">
+          Provides a secure income for life?
+          </th>
+          <td data-title="Annuities">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Cash lump sums">
+            <span aria-label="cross" class="icon-cross"></span>
+          </td>
+          <td data-title="Drawdown">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Taking your whole pot as cash">
+            <span aria-label="cross" class="icon-cross"></span>
+          </td>
+        </tr>
+        <tr>
+          <th colspan="2">
+          Allows you to change your income?
+          </th>
+          <td data-title="Annuities">
+            <span aria-label="cross" class="icon-cross"></span>
+          </td>
+          <td data-title="Cash lump sums">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Drawdown">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Taking your whole pot as cash">
+            <span aria-label="cross" class="icon-cross"></span>
+          </td>
+        </tr>
+        <tr>
+          <th colspan="2">
+          Is your remaining pot still invested?
+          </th>
+          <td data-title="Annuities">
+            <span aria-label="cross" class="icon-cross"></span>
+          </td>
+          <td data-title="Cash lump sums">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Drawdown">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Taking your whole pot as cash">
+            <span aria-label="cross" class="icon-cross"></span>
+          </td>
+        </tr>
+        <tr>
+          <th colspan="2">
+          Affected by the stock market?
+          </th>
+          <td data-title="Annuities">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Cash lump sums">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Drawdown">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Taking your whole pot as cash">
+            <span aria-label="cross" class="icon-cross"></span>
+          </td>
+        </tr>
+      </tbody>
+      </table>
     </div>
   `,
 });
 
-export const style4 = () => ({
+export const alternate_columns = () => ({
   template: `
-    <div class="container">
-      <div class="row">
-        <div class="col-10">
-
-          <table class="wes-table-style-4">
-            <thead>
-              <tr>
-                <th>Cookies</th>
-                <th>Purpose</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>SC_ANALYTICS_&#8203;GLOBAL_COOKIE</td>
-                <td>Preserves the visitor's session state across page requests. This cookie expires after the browser session. This is a first party cookie. </td>
-              </tr>
-              <tr>
-                <td>_gtm</td>
-                <td>Used by Google Tag Manager to load other scripts and code into a page. It is regarded as Strictly Necessary as, without it, other scripts may not function correctly. This cookie expires after the browser session. This is a first party cookie.</td>
-              </tr>
-              <tr>
-                <td>sxa_site</td>
-                <td>Used by our content management system (Sitecore) to identify the visitor's session state across page requests on the server. This cookie expires after the browser session. This is a first party cookie.</td>
-              </tr>
-              <tr>
-                <td>SyrenisCookiexxxx</td>
-                <td>Used by our consent management system (Cassie) to enable us to capture users' preferences compliantly under regulatory frameworks.</td>
-              </tr>
-            </tbody>
-          </table>
-
-        </div>
-      </div>
+    <div class="container wes-table-container">
+      <table class="wes-table transparent alternate-gold-columns">
+      <caption>England and Wales NHS pension contributions</caption>
+      <thead>
+        <tr>
+          <th colspan="2"></th>
+          <th>Annuities</th>
+          <th>Cash lump sums</th>
+          <th>Drawdown</th>
+          <th>Taking your whole pot as cash</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th colspan="2">
+            Provides an income?
+          </th>
+          <td data-title="Annuities">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Cash lump sums">
+            <span aria-label="cross" class="icon-cross"></span>
+          </td>
+          <td data-title="Drawdown">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Taking your whole pot as cash">
+            <span aria-label="cross" class="icon-cross"></span>
+          </td>
+        </tr>
+        <tr>
+          <th colspan="2">
+          Provides a secure income for life?
+          </th>
+          <td data-title="Annuities">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Cash lump sums">
+            <span aria-label="cross" class="icon-cross"></span>
+          </td>
+          <td data-title="Drawdown">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Taking your whole pot as cash">
+            <span aria-label="cross" class="icon-cross"></span>
+          </td>
+        </tr>
+        <tr>
+          <th colspan="2">
+          Allows you to change your income?
+          </th>
+          <td data-title="Annuities">
+            <span aria-label="cross" class="icon-cross"></span>
+          </td>
+          <td data-title="Cash lump sums">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Drawdown">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Taking your whole pot as cash">
+            <span aria-label="cross" class="icon-cross"></span>
+          </td>
+        </tr>
+        <tr>
+          <th colspan="2">
+          Is your remaining pot still invested?
+          </th>
+          <td data-title="Annuities">
+            <span aria-label="cross" class="icon-cross"></span>
+          </td>
+          <td data-title="Cash lump sums">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Drawdown">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Taking your whole pot as cash">
+            <span aria-label="cross" class="icon-cross"></span>
+          </td>
+        </tr>
+        <tr>
+          <th colspan="2">
+          Affected by the stock market?
+          </th>
+          <td data-title="Annuities">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Cash lump sums">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Drawdown">
+            <span aria-label="tick" class="icon-tick"></span>
+          </td>
+          <td data-title="Taking your whole pot as cash">
+            <span aria-label="cross" class="icon-cross"></span>
+          </td>
+        </tr>
+      </tbody>
+      </table>
     </div>
   `,
 });
