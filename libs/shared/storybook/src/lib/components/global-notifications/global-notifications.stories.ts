c247bfeb909e51f94665fc48dc435ddd0ca8aac5
Femi Ojemuyiwa <ojemuyiwa@gmail.com>
2021-02-03 16:27:46 +0000

    feat: mark-up and styling complete for global-notifications and single-page-notifications

--- libs/shared/storybook/src/lib/components/global-notifications/global-notifications.stories.ts
+++ libs/shared/storybook/src/lib/components/global-notifications/global-notifications.stories.ts
@@ -1,18 +1,50 @@
+import { select, text, withKnobs, optionsKnob } from '@storybook/addon-knobs';
 export default {
   title: 'Components/Global notifications',
+  decorators: [withKnobs],
+  parameters: {
+    knobs: {
+      escapeHTML: false,
+    },
+  },
 };
 
-export const emergencyBroadcast = () => ({
-  template: `<div class="wes-global-notification fade show" role="alert" aria-live="assertive" aria-atomic="true">
-  <div class="wes-global-notification__header">
-    <span class="me-auto">Our phone lines are down…</span>
-    <button type="button" class="wes-global-notification__close" data-bs-dismiss="wes-global-notification" aria-label="Close"></button>
-  </div>
-  <div class="wes-global-notification__body">
-    Trying to call us? We’re having some technical problems. Find out more.
-  </div>
-  <div class="wes-global-notification__timestamp">
-    Updated 18th January 2021, 4pm
+const styleVariants = {
+  'Emergency Broadcast': '',
+  'Single Page': 'single-page',
+};
+
+export const notifications = () => ({
+  template: `<div class="wes-global-notification fade show {{variant}}" role="alert" aria-live="assertive" aria-atomic="true">
+  <div class="container">
+    <div class="wes-global-notification__header">
+      <span class="me-auto">{{headerText}}</span>
+      <button *ngIf="variant !== 'single-page'"  type="button" class="wes-global-notification__close" data-bs-dismiss="wes-global-notification" aria-label="Close"></button>
+    </div>
+    <div *ngIf="bodyText.length > 0" class="wes-global-notification__body" [innerHtml]="bodyText"></div>
+    <div *ngIf="timeStamp.length > 0 && withDateStamp == 'yes'" class="wes-global-notification__timestamp">
+      {{timeStamp}}
+    </div>
   </div>
 </div>`,
+  props: {
+    variant: select('Variant', styleVariants, ''),
+    headerText: text('header text', 'Our phone lines are down…'),
+    bodyText: text(
+      'body text',
+      `Trying to call us? We’re having some technical problems. <a href="javascript:void(0);">Find out more</a>`
+    ),
+    timeStamp: text('date stamp', 'Updated 18th January 2021, 4pm'),
+    withDateStamp: optionsKnob(
+      'with date stamp',
+      {
+        Yes: 'yes',
+        No: 'no',
+      },
+      'yes',
+      {
+        display: 'inline-radio',
+      }
+    ),
+  },
 });
