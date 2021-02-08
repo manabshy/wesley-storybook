f69cf9420a7418c26f23bbfb1f3cb11f367ab76b
Femi Ojemuyiwa <ojemuyiwa@gmail.com>
2021-02-08 20:19:48 +0000

    feat: logic applied for media-match

--- libs/shared/storybook/src/lib/components/tabs/tabs.stories.ts
+++ libs/shared/storybook/src/lib/components/tabs/tabs.stories.ts
@@ -2,20 +2,12 @@ import { addParameters, storiesOf } from '@storybook/angular';
 import { array, select, object, text, withKnobs } from '@storybook/addon-knobs';
 import addons from '@storybook/addons';
 import jquery from 'jquery';
-import util from 'bootstrap/js/dist/util';
-import tab from 'bootstrap/js/dist/tab';
+import * as bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min';
 // import 'bootstrap/js/dist/dropdown';
 
-global = { ...global, ...{ $: jquery, jQuery: jquery, Util: util, Tab: tab } };
-
-console.log('global:', global);
+global = { ...global, ...{ $: jquery, jQuery: jquery, bootstrap: bootstrap } };
 
 addParameters({ docs: { iframeHeight: 500 } });
-
-// storiesOf('Tabs', module).addDecorator(story => {
-//     addons.getChannel().emit(CoreEvents.REGISTER_SUBSCRIPTION, subscribe));
-//     return story();
-//   });
 
 export default {
   title: 'Components/Tabs',
@@ -28,11 +20,14 @@ const styleVariants = {
 };
 
 const tabScript = () => {
+  // get data attribute for media match
+
+  // check state and execute tabs or accordion accordingly
   var triggerTabList = [].slice.call(
-    document.querySelectorAll('a[data-bs-toggle]')
+    document.querySelectorAll('a[data-toggle]')
   );
   triggerTabList.forEach(function (triggerEl) {
-    var tabTrigger = new tab(triggerEl);
+    var tabTrigger = new bootstrap.Tab(triggerEl);
 
     triggerEl.addEventListener('click', function (event) {
       event.preventDefault();
@@ -90,28 +85,88 @@ const tabScript = () => {
 
 export const tabs1 = () => ({
   template: `
+  <div class="container wes-tabs">
     <!-- Nav tabs -->
-<ul class="nav nav-tabs" id="myTab" role="tablist">
-  <li class="nav-item" role="presentation">
-    <a class="nav-link active" id="home-tab" data-bs-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
-  </li>
-  <li class="nav-item" role="presentation">
-    <a class="nav-link" id="profile-tab" data-bs-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
-  </li>
-  <li class="nav-item" role="presentation">
-    <a class="nav-link" id="messages-tab" data-bs-toggle="tab" href="#messages" role="tab" aria-controls="messages" aria-selected="false">Messages</a>
-  </li>
-  <li class="nav-item" role="presentation">
-    <a class="nav-link" id="settings-tab" data-bs-toggle="tab" href="#settings" role="tab" aria-controls="settings" aria-selected="false">Settings</a>
-  </li>
-</ul>
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+      <li class="nav-item" role="presentation">
+        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+      </li>
+      <li class="nav-item" role="presentation">
+        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+      </li>
+      <li class="nav-item" role="presentation">
+        <a class="nav-link" id="messages-tab" data-toggle="tab" href="#messages" role="tab" aria-controls="messages" aria-selected="false">Messages</a>
+      </li>
+      <li class="nav-item" role="presentation">
+        <a class="nav-link" id="settings-tab" data-toggle="tab" href="#settings" role="tab" aria-controls="settings" aria-selected="false">Settings</a>
+      </li>
+    </ul>
 
-<!-- Tab panes -->
-<div class="tab-content">
-  <div class="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab">home tabs content</div>
-  <div class="tab-pane" id="profile" role="tabpanel" aria-labelledby="profile-tab">profile tabs content</div>
-  <div class="tab-pane" id="messages" role="tabpanel" aria-labelledby="messages-tab">messages tabs content</div>
-  <div class="tab-pane" id="settings" role="tabpanel" aria-labelledby="settings-tab">setttings tabs content</div>
-</div>
+    <!-- Tab panes -->
+    <div class="tab-content">
+      <div class="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab">
+        <div class="panel-heading">
+          <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent=".tab-pane" href="#collapse1">
+              Home
+            </a>
+          </h4>
+        </div>
+        <div id="collapse1" class="tab-panel-body collapse">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin mollis vehicula arcu id maximus. Nam finibus eget arcu sit amet interdum. Sed at tellus sem. Suspendisse maximus mollis hendrerit. Quisque venenatis quam at tellus pretium ornare. Morbi aliquam, turpis vel cursus iaculis, purus lectus dictum velit, in placerat purus neque non est. Phasellus dui risus, consequat et gravida eget, pharetra ac nunc. Sed in augue sodales odio laoreet tincidunt id eget lacus. In id nulla eget lectus euismod fermentum vulputate fermentum metus. Aenean ac lectus erat. Quisque at orci orci.
+        </div>
+      </div>
+      <div class="tab-pane" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+        <div class="panel-heading">
+          <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent=".tab-pane" href="#collapse2">
+              Profile
+            </a>
+          </h4>
+        </div>
+        <div id="collapse2" class="tab-panel-body collapse">
+          Nulla est ullamco ut irure incididunt nulla Lorem Lorem minim irure officia enim reprehenderit. Magna duis labore cillum sint adipisicing exercitation ipsum. Nostrud ut anim non exercitation velit laboris fugiat cupidatat. Commodo esse dolore fugiat sint velit ullamco magna consequat voluptate minim amet aliquip ipsum aute laboris nisi. Labore labore veniam irure irure ipsum pariatur mollit magna in cupidatat dolore magna irure esse tempor ad mollit. Dolore commodo nulla minim amet ipsum officia consectetur amet ullamco voluptate nisi commodo ea sit eu.
+        </div>
+      </div>
+      <div class="tab-pane" id="messages" role="tabpanel" aria-labelledby="messages-tab">
+        <div class="panel-heading">
+          <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent=".tab-pane" href="#collapse3">
+              Messages
+            </a>
+          </h4>
+        </div>
+        <div id="collapse3" class="tab-panel-body collapse">
+          Morbi in pellentesque tortor. Mauris metus ipsum, luctus sed purus vel, faucibus sollicitudin nibh. Donec suscipit lacinia libero, a condimentum dolor tincidunt ac. Phasellus gravida, risus sit amet semper venenatis, tellus purus fermentum enim, vel ullamcorper dui nisl non ligula. Phasellus dignissim vehicula magna, sed ultrices arcu. Suspendisse tortor nulla, dictum et odio at, tincidunt laoreet felis. Quisque quis dui dui. Donec in velit mollis, feugiat augue id, efficitur neque.
+        </div>
+      </div>
+      <div class="tab-pane" id="settings" role="tabpanel" aria-labelledby="settings-tab">
+        <div class="panel-heading">
+          <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent=".tab-pane" href="#collapse4">
+              Settings
+            </a>
+          </h4>
+        </div>
+        <div id="collapse4" class="tab-panel-body collapse">
+        Vestibulum scelerisque eleifend augue, in faucibus massa tempus et. Sed id neque nulla. In a massa maximus, tempus lectus non, porttitor purus. Quisque vestibulum suscipit vehicula. Suspendisse iaculis, nisl in congue hendrerit, purus ipsum blandit ligula, eu cursus quam neque ut sapien. Phasellus hendrerit lorem velit, eu semper sapien volutpat auctor. Proin enim metus, pellentesque in euismod vel, hendrerit a lorem. Ut viverra magna ut diam rhoncus, vel pretium arcu porta. Donec sodales feugiat augue, quis blandit sem finibus ac. Ut ac nisi vel urna euismod scelerisque. Ut vehicula tempor justo. Aenean posuere, mi sed molestie tempor, purus nisi rutrum eros, in tempus ex ligula nec sem. In rutrum lorem in eros gravida, id convallis orci tincidunt. Pellentesque feugiat blandit nulla nec vehicula.
+        </div>
+      </div>
+    </div>
+  </div>
     `,
 });
+
+// tabs1.decorators = [
+//   (storyFunc) => {
+//     const story = storyFunc();
+//     // setTimeout(() => {
+//     //   tabScript();
+//     // });
+
+//     return {
+//       ...story,
+//       template: `${story.template}`,
+//     };
+//   },
+// ];
