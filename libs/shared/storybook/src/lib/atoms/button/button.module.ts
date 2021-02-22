e708de0e76352c22e1170dc997d8412b338ebac0
Femi Ojemuyiwa <ojemuyiwa@gmail.com>
2021-02-22 17:20:37 +0000

    fix: create icons for FC details re-worked button right and left arrows. #91413

--- libs/shared/storybook/src/lib/atoms/button/button.module.ts
+++ libs/shared/storybook/src/lib/atoms/button/button.module.ts
@@ -1,31 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule  } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faBars,
-  faBell,
-  faCalendar
-
-} from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [ButtonComponent],
-  imports:[
-    CommonModule,
-    FontAwesomeModule
-  ],
-  exports: [ButtonComponent]
+  imports: [CommonModule],
+  exports: [ButtonComponent],
 })
-
 export class ButtonModule {
-
-  constructor() {
-    library.add(
-      faBars,
-      faBell,
-      faCalendar,
-    );
-  }
+  constructor() {}
 }
