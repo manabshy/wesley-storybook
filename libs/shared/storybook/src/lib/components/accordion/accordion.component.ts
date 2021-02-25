8092fdab083de019d02f5fb5310d2298ab1b2c67
Femi Ojemuyiwa <ojemuyiwa@gmail.com>
2021-02-25 15:42:02 +0000

    fix: adding bg to host

--- libs/shared/storybook/src/lib/components/accordion/accordion.component.ts
+++ libs/shared/storybook/src/lib/components/accordion/accordion.component.ts
@@ -2,6 +2,7 @@ import {
   Component,
   ElementRef,
   EventEmitter,
+  HostBinding,
   Input,
   Output,
   TemplateRef,
@@ -22,7 +23,15 @@ export class AccordionComponent {
   @Input() background = 'wes-bg-solid-white';
   @Input() idAttribute: string = 'frequentlyAskedQuestions';
   @Input() title: string = 'Frequently Asked Questions';
+  private colours = {
+    'wes-bg-solid-white': '#ffffff;',
+    'wes-bg-solid-grey-50': '#f6f6f6',
+  };
   constructor() {}
+
+  @HostBinding('style.background-color') public color: string = this.colours[
+    this.background
+  ];
 
   toggleMenu(event: Event) {
     const toggleIcon = (event.currentTarget as HTMLElement).parentElement;
