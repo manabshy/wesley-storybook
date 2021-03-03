ac1da6562168537d26c8463c5ce42f8c4d09cde7
Femi Ojemuyiwa <ojemuyiwa@gmail.com>
2021-03-03 20:52:45 +0000

    fix: accordion rework #91782

--- libs/shared/storybook/src/lib/components/accordion/accordion.component.ts
+++ libs/shared/storybook/src/lib/components/accordion/accordion.component.ts
@@ -1,10 +1,6 @@
 import {
   Component,
-  ElementRef,
-  EventEmitter,
-  HostBinding,
   Input,
-  Output,
   TemplateRef,
   ViewChild,
   ViewEncapsulation,
@@ -23,15 +19,7 @@ export class AccordionComponent {
   @Input() background = 'wes-bg-solid-white';
   @Input() idAttribute: string = 'frequentlyAskedQuestions';
   @Input() title: string = 'Frequently Asked Questions';
-  private colours = {
-    'wes-bg-solid-white': '#ffffff;',
-    'wes-bg-solid-grey-50': '#f6f6f6',
-  };
   constructor() {}
-
-  @HostBinding('style.background-color') public color: string = this.colours[
-    this.background
-  ];
 
   toggleMenu(event: Event) {
     const toggleIcon = (event.currentTarget as HTMLElement).parentElement;
