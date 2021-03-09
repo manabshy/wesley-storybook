37a20310d8763075dad9890fb010db7218563302
Femi Ojemuyiwa <ojemuyiwa@gmail.com>
2021-03-09 21:54:59 +0000

    feat: in page menu complete #89342

--- libs/shared/storybook/src/lib/components/in-page-menu/in-page-menu.stories.ts
+++ libs/shared/storybook/src/lib/components/in-page-menu/in-page-menu.stories.ts
@@ -1,4 +1,3 @@
-import { addParameters } from '@storybook/angular';
 import {
   select,
   text,
@@ -7,51 +6,60 @@ import {
   object,
 } from '@storybook/addon-knobs';
 
-addParameters({ docs: { iframeHeight: 500 } });
-
 export default {
   title: 'Components/InPageMenu',
   decorators: [withKnobs],
-  //   parameters: {
-  //     knobs: {
-  //       escapeHTML: false,
-  //     },
-  //   },
+  parameters: {
+    knobs: {
+      escapeHTML: false,
+    },
+    docs: { iframeHeight: 500 },
+  },
 };
 
 const styleVariants = {
-  'white background': 'container-white',
-  'grey background': 'container-grey',
+  'white background': 'wes-bg-solid-white',
+  'grey background': 'wes-bg-solid-grey-50',
 };
 
-export const inPageMenu = () => ({
+export const inpagemenu = () => ({
   template: `
-  <nav aria-label="in page menu" class="{{variant}}">
-    <h2>{{mainHeading}}</h2>
-    <ul>
-        <ng-container *ngFor="let menuItem of menuItems">
-            <li [ngClass]="(showIcons==='yes' && menuItem.iconcss) ? menuItem.iconcss : null">{{menuItem.text}}</li>
-        </ng-container>
-    </ul>
+  <nav aria-label="in page menu" class="wes-in-page-menu container {{variant}}">
+    <div class="row">
+        <div class="col-12">
+            <h2 class="heading">{{mainHeading}}</h2>
+            <ul>
+                <ng-container *ngFor="let menuItem of menuItems">
+                    <li><a [ngClass]="(showIcons==='yes' && menuItem.iconcss) ? menuItem.iconcss : null" href="#">{{menuItem.text}}</a></li>
+                </ng-container>
+            </ul>
+        </div>
+    </div>
   </nav>
   `,
-  variant: select('variant', styleVariants, 'container-white'),
-  mainHeading: text('main heading', 'On this page'),
-  menuItems: object('menu items', [
-    { text: 'How it works', iconcss: '' },
-    { text: 'Our funds', iconcss: '' },
-    { text: 'Key features', iconcss: '' },
-    { text: 'Apply for your ISA', iconcss: '' },
-  ]),
-  showIcons: optionsKnob(
-    'show icons',
-    {
-      Yes: 'yes',
-      No: 'no',
-    },
-    'yes',
-    {
-      display: 'inline-radio',
-    }
-  ),
+  props: {
+    variant: select('variant', styleVariants, 'wes-bg-solid-white'),
+    mainHeading: text('main heading', 'On this page'),
+    menuItems: object('menu items', [
+      { text: 'How it works', iconcss: 'wes-font-icon-chat' },
+      { text: 'Our funds', iconcss: 'wes-font-icon-user' },
+      { text: 'Key features', iconcss: 'wes-font-icon-alert' },
+      { text: 'Apply for your ISA', iconcss: 'wes-font-icon-clock' },
+      {
+        text: 'What’s Total Home Protection?',
+        iconcss: 'wes-font-icon-chevron-down',
+      },
+    ]),
+    showIcons: optionsKnob(
+      'show icons',
+      {
+        Yes: 'yes',
+        No: 'no',
+      },
+      'yes',
+      {
+        display: 'inline-radio',
+      }
+    ),
+  },
 });
