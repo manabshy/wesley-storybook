2d6f8a598c1523c9ff0518d23d73f6352af3453b
Femi Ojemuyiwa <ojemuyiwa@gmail.com>
2021-03-09 17:30:02 +0000

    initial checkin

---
+++ libs/shared/storybook/src/lib/components/in-page-menu/in-page-menu.stories.ts
@@ -0,0 +1,59 @@
+import { addParameters, moduleMetadata } from '@storybook/angular';
+import {
+  array,
+  select,
+  text,
+  withKnobs,
+  optionsKnob,
+  object,
+} from '@storybook/addon-knobs';
+import { Pipe, PipeTransform } from '@angular/core';
+
+addParameters({ docs: { iframeHeight: 500 } });
+
+export default {
+  title: 'Components/InPageMenu',
+  decorators: [withKnobs],
+  parameters: {
+    knobs: {
+      escapeHTML: false,
+    },
+  },
+};
+
+const styleVariants = {
+  'white background': 'container-white',
+  'grey background': 'container-grey',
+};
+
+export const inPageMenu = () => ({
+  template: `
+  <nav aria-label="in page menu">
+    <h2>{{mainHeading}}</h2>
+    <ul>
+        <ng-container *ngFor="let menuItem of menuItems">
+            <li [ngClass]="(showIcons==='yes' && menuItem.iconcss) ? menuItem.iconcss : null">{{menuItem.text}}</li>
+        </ng-container>
+    </ul>
+  </nav>
+  `,
+  variant: select('variant', styleVariants, 'container-white'),
+  mainHeading: text('main heading', 'On this page'),
+  menuItems: object('menu items', [
+    { text: 'How it works', iconcss: '' },
+    { text: 'Our funds', iconcss: '' },
+    { text: 'Key features', iconcss: '' },
+    { text: 'Apply for your ISA', iconcss: '' },
+  ]),
+  showIcons: optionsKnob(
+    'show icons',
+    {
+      Yes: 'yes',
+      No: 'no',
+    },
+    'yes',
+    {
+      display: 'inline-radio',
+    }
+  ),
+});
