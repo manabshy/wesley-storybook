9c3845c3ff612b093730d17ce23241086f9bc33e
Charl Gottschalk <2204385+CharlGottschalk@users.noreply.github.com>
2021-03-10 10:56:52 +0200

    Added right alignment

--- libs/shared/storybook/src/lib/components/core/content-block/content-block.stories.ts
+++ libs/shared/storybook/src/lib/components/core/content-block/content-block.stories.ts
@@ -39,55 +39,6 @@ export const contentBlock = () => ({
                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                   </span>
 
-                  <h2 class="level2">CTA Card</h2>
-                  <div class="wes-cta-card">
-                  <div class="wes-cta-card__wrapper">
-                  <div class="wes-cta-card__frame">
-                     <div class="wes-cta-card__body">
-                        <div class="wes-cta-card__body-content">
-                        <div class="wes-cta-card__icon">
-                           <svg height="56" width="45" xmlns="http://www.w3.org/2000/svg">
-                              <g fill="none" fill-rule="evenodd">
-                              <path
-                                 d="M1 46.5h42.5"
-                                 stroke="#FEBD11"
-                                 stroke-linecap="square"
-                              ></path>
-                              <path
-                                 d="M25.5.5v22.557L18 17.969l-7.5 5.088V.5h15z"
-                                 stroke="#FEBD11"
-                              ></path>
-                              <path
-                                 d="M.5.5h42a2 2 0 012 2v51a2 2 0 01-2 2H.5h0V.5z"
-                                 stroke="#1E2630"
-                                 stroke-linecap="round"
-                              ></path>
-                              <path
-                                 d="M35.5.5v54"
-                                 stroke="#1E2630"
-                                 stroke-linecap="square"
-                              ></path>
-                              </g>
-                           </svg>
-                        </div>
-                        <div class="wes-cta-card__content">
-                           <div class="wes-cta-card__message">
-                              <p>
-                              Book today and we'll also send you our free guide to the NHS
-                              pension - packed with useful information to help you make sense
-                              of the scheme.
-                              </p>
-                           </div>
-                           <div class="wes-cta-card__action">
-                              <a class="wes-link" href="#">Get your free NHS pension guide</a>
-                           </div>
-                        </div>
-                        </div>
-                     </div>
-                  </div>
-                  </div>
-                  </div>
-
                  <h2 class="level2">L2 - What is a stocks and shares ISA?</h2>
 
                  <table class="wes-core-table transparent">
