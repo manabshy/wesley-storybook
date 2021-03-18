8d878b317c926336a7abc5c51043f82dcd8c9dec
Femi Ojemuyiwa <ojemuyiwa@gmail.com>
2021-03-18 10:10:35 +0000

    feat: header nav initial commit #90782

--- libs/shared/storybook/src/lib/components/campaigns/campaign-header/campaign-header.stories.ts
+++ libs/shared/storybook/src/lib/components/campaigns/campaign-header/campaign-header.stories.ts
@@ -1,133 +1,56 @@
 export default {
   title: 'Campaigns/Header',
 };
-  
+
 export const base = () => ({
   template: `
-    <header class="c-header__main wes-campaign-header">
-      <div class="wes-campaign-header__body">
-        <a href="https://www.wesleyan.co.uk" class="wes-campaign-header__logo c-logo">
-          <span class="sr-only">Wesleyan</span>
-          <svg
-            height="30px"
-            viewBox="0 0 260 30"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-          >
-            <defs>
-              <polygon
-                id="logo-path-1"
-                points="38.281 29.94 0.775 29.94 0.775 0.425 38.281 0.425"
-              ></polygon>
-            </defs>
-            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-              <g>
-                <g>
-                  <path
-                    d="M30.322,29.737 L26.597,29.737 L21.733,16.286 C21.295,15.016 20.771,13.526 20.332,11.992 L20.244,11.992 C19.805,13.526 19.323,14.972 18.841,16.242 L13.891,29.737 L10.252,29.737 L-6.99884595e-13,0.687 L5.082,0.687 L10.998,18.697 C11.48,20.141 11.875,21.5 12.269,22.901 L12.356,22.901 C12.707,21.544 13.189,20.186 13.714,18.741 L18.186,6.163 L16.213,0.687 L20.901,0.687 L27.035,18.389 C27.605,20.01 28.13,21.367 28.481,22.771 L28.613,22.771 C28.919,21.323 29.27,20.054 29.753,18.519 L35.404,0.687 L40.136,0.687 L30.322,29.737"
-                    fill="#50504D"
-                  ></path>
-                  <g transform="translate(43.000000, 0.000000)">
-                    <mask id="logo-mask-2" fill="white">
-                      <use xlink:href="#logo-path-1"></use>
-                    </mask>
-                    <g></g>
-                    <polyline
-                      fill="#50504D"
-                      mask="url(#logo-mask-2)"
-                      points="0.775 29.563 0.775 0.686 16.198 0.686 14.797 4.63 5.377 4.63 5.377 12.824 14.753 12.824 14.753 16.768 5.377 16.768 5.377 25.618 16.241 25.618 16.241 29.563 0.775 29.563"
-                    ></polyline>
-                    <path
-                      d="M28.423,30 C24.436,30 21.72,28.687 19.878,27.502 L21.807,24.041 C23.254,25.05 25.53,26.189 28.248,26.189 C30.614,26.189 33.376,25.136 33.376,22.507 C33.376,18.082 26.408,17.206 22.903,13.307 C21.72,11.992 20.931,10.372 20.931,8.135 C20.931,3.929 24.173,0.425 30.001,0.425 C33.069,0.425 35.347,1.214 36.836,2.001 L36.793,6.295 C34.82,5.113 32.585,4.281 30.219,4.281 C27.722,4.281 25.664,5.331 25.664,7.609 C25.664,13.307 38.281,13.131 38.281,21.982 C38.281,26.845 33.988,30 28.423,30"
-                      fill="#50504D"
-                      mask="url(#logo-mask-2)"
-                    ></path>
-                  </g>
-                  <polyline
-                    fill="#50504D"
-                    points="86.059 29.563 86.059 0.686 90.615 0.686 90.615 25.618 101.175 25.618 99.998 29.563 86.059 29.563"
-                  ></polyline>
-                  <polyline
-                    fill="#50504D"
-                    points="104.267 29.563 104.267 0.686 119.691 0.686 118.291 4.63 108.869 4.63 108.869 12.824 118.247 12.824 118.247 16.768 108.869 16.768 108.869 25.618 119.735 25.618 119.735 29.563 104.267 29.563"
-                  ></polyline>
-                  <path
-                    d="M136.56,18.082 L136.56,29.564 L131.959,29.564 L131.959,17.994 L122.145,0.687 L127.227,0.687 L132.003,9.319 C132.792,10.722 133.711,12.43 134.326,13.833 L134.414,13.833 C135.072,12.43 135.905,10.766 136.65,9.406 L141.377,0.687 L146.2,0.687 L136.56,18.082"
-                    fill="#50504D"
-                  ></path>
-                  <path
-                    d="M164.512,29.563 L161.62,22.202 L150.929,22.202 L148.211,29.563 L143.654,29.563 L154.827,0.599 L158.025,0.599 L169.462,29.563 L164.512,29.563 Z M157.458,10.633 C156.974,9.187 156.58,8.005 156.406,7.084 L156.362,7.084 C156.098,8.092 155.704,9.232 155.222,10.589 L152.375,18.345 L160.129,18.345 L157.458,10.633 L157.458,10.633 Z"
-                    fill="#50504D"
-                  ></path>
-                  <path
-                    d="M194.133,29.563 L180.55,11.817 C179.147,9.975 178.358,8.837 177.656,7.654 L177.57,7.654 C177.656,8.619 177.656,10.021 177.656,11.421 L177.656,29.563 L173.102,29.563 L173.102,0.686 L177.746,0.686 L190.803,17.775 C192.378,19.835 193.167,20.931 193.826,22.112 L193.913,22.112 C193.869,20.931 193.826,19.484 193.826,18.081 L193.826,0.686 L198.425,0.686 L198.425,29.563 L194.133,29.563"
-                    fill="#50504D"
-                  ></path>
-                </g>
-              </g>
-            </g>
-          </svg>
-        </a>
-
-        <div class="wes-campaign-header__actions">
-          <a class="wes-campaign-header__cta wes-button wes-button-cta wes-button-cta--orange-with-grey-hover" href="#">
-            <span class="wes-button-label">Speak to our experts</span>
-            <svg
-              xmlns:xlink="http://www.w3.org/1999/xlink"
-              class="wes-icon wes-icon-size-s"
-              version="1.1"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g
-                fill="none"
-                fill-rule="evenodd"
-                stroke="none"
-                stroke-linecap="round"
-                stroke-width="1"
-              >
-                <g
-                  stroke="#40474f"
-                  stroke-width="2"
-                  transform="translate(-293.000000, -340.000000)"
-                >
-                  <g transform="translate(0.000000, 72.000000)">
-                    <g transform="translate(97.000000, 88.000000)">
-                      <g transform="translate(0.000000, 160.000000)">
-                        <g transform="translate(32.000000, 20.000000)">
-                          <g transform="translate(164.000000, 0.000000)">
-                            <polyline
-                              stroke="#40474f"
-                              class="svg-path-color-change"
-                              id="Rectangle"
-                              points="25 9 19 15 13 9"
-                              stroke-linejoin="round"
-                              transform="translate(19.000000, 12.000000) rotate(-90.000000) translate(-19.000000, -12.000000) "
-                            ></polyline>
-                            <line
-                              stroke="#40474f"
-                              class="svg-path-color-change"
-                              id="Line"
-                              x1="21"
-                              x2="2"
-                              y1="12"
-                              y2="12"
-                            ></line>
-                          </g>
-                        </g>
-                      </g>
-                    </g>
-                  </g>
-                </g>
-              </g>
-            </svg>
-          </a>
+  <header>
+    <nav class="wes-header">
+      <!-- topnavbar//-->
+      <div class="wes-bg-solid-grey-900 topnavbar">
+        <div class="container">
+          <div class="d-flex flex-row">
+            <div class="d-flex" role="tablist">
+              <a href="#" role="tab" data-text="Personal">Personal</a>
+              <a href="#" role="tab" data-text="Business">Business</a>
+            </div>  
+            <div class="mr-auto"></div>
+            <div class="d-none d-xl-flex" role="list">
+              <a href="#" role="listitem" data-text="Members area">Members area</a>
+              <a href="#" role="listitem" data-text="Fund prices">Fund prices</a>
+              <a href="#" role="listitem" data-text="Make a claim">Make a claim</a>
+              <a href="#" role="listitem" class="login" data-text="Login">Login</a>
+            </div>
+          </div>
         </div>
       </div>
-    </header>
+      <!-- end topnavbar//-->
 
-
+      <!-- masthead//-->
+      <div class="wes-bg-solid-white masthead">
+        <div class="container">
+          <div class="d-flex flex-row align-items-center" role="list">
+            <div class="d-flex">
+              <a href="#" aria-label="wesleyan logo" class="wes-logo"></a>
+            </div>
+            <div class="mr-auto"></div>
+            <div class="d-flex d-xl-none flex-wrap" role="group">
+              <span class="menu-item" role="listitem">
+              <a class="d-flex flex-column align-items-center wes-font-icon-user" href="#">Login</a>
+              </span>
+              <span class="menu-item" role="listitem">
+              <a class="d-flex flex-column align-items-center wes-font-icon-search" href="#">Search</a>
+              </span>
+              <span class="menu-item" role="listitem">
+              <a class="d-flex flex-column align-items-center wes-font-icon-menu" href="#">Menu</a>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- end masthead//-->
+    </nav>
+  </header>
     <!--
     <script>
         window.addEventListener('DOMContentLoaded', function (event) {
