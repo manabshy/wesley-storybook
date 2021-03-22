5dc7267c29334cd1e3684975420058221d6f3980
Femi Ojemuyiwa <ojemuyiwa@gmail.com>
2021-03-22 10:09:31 +0000

    feat: accessibility revision and skip to content added

--- libs/shared/storybook/src/lib/components/campaigns/campaign-header/campaign-header.stories.ts
+++ libs/shared/storybook/src/lib/components/campaigns/campaign-header/campaign-header.stories.ts
@@ -18,22 +18,27 @@ export default {
 
 export const header = () => ({
   template: `
+  <a class="sr-only sr-only-focusable skip-to-content" href="#content">
+    <div class="container">
+      <span class="skiplink-text">Skip to main content</span>
+    </div>
+  </a>
   <header>
     <nav class="wes-header">
       <!-- topnavbar//-->
       <div class="wes-bg-solid-grey-900 topnavbar">
         <div class="container">
           <div class="d-flex flex-row">
-            <div class="d-flex" role="tablist">
-              <a href="#" role="tab" data-text="Personal">Personal</a>
-              <a href="#" role="tab" data-text="Business">Business</a>
+            <div class="d-flex">
+              <a href="#" data-text="Personal">Personal</a>
+              <a href="#" data-text="Business">Business</a>
             </div>  
             <div class="mr-auto"></div>
-            <div class="d-none d-xl-flex" role="list">
-              <a href="#" role="listitem" data-text="Members area">Members area</a>
-              <a href="#" role="listitem" data-text="Fund prices">Fund prices</a>
-              <a href="#" role="listitem" data-text="Make a claim">Make a claim</a>
-              <a href="#" role="listitem" class="login" data-text="Login">Login</a>
+            <div class="d-none d-xl-flex">
+              <a href="#" data-text="Members area">Members area</a>
+              <a href="#" data-text="Fund prices">Fund prices</a>
+              <a href="#" data-text="Make a claim">Make a claim</a>
+              <a href="#" class="login" data-text="Login">Login</a>
             </div>
           </div>
         </div>
@@ -43,7 +48,7 @@ export const header = () => ({
       <!-- masthead//-->
       <div class="wes-bg-solid-white masthead">
         <div class="container">
-          <div class="d-flex flex-row align-items-center" role="list">
+          <div class="d-flex flex-row align-items-center">
             <div class="d-flex">
               <a href="#" aria-label="wesleyan logo" class="wes-logo">
                 <span class="sr-only">Wesleyan</span>
@@ -51,31 +56,31 @@ export const header = () => ({
             </div>
             <div class="mr-auto"></div>
             <!-- start mobile to large masthead //-->
-            <div class="d-flex d-xl-none flex-wrap" role="group">
-              <span class="menu-item" role="listitem">
+            <div class="d-flex d-xl-none flex-wrap">
+              <span class="menu-item">
               <a class="d-flex flex-column align-items-center wes-font-icon-user" href="#">Login</a>
               </span>
-              <span class="menu-item" role="listitem">
+              <span class="menu-item">
               <a class="d-flex flex-column align-items-center wes-font-icon-search" data-toggle="collapse" href="#searchPanel" role="button" aria-expanded="false" aria-controls="searchPanel">Search</a>
               </span>
-              <span class="menu-item" role="listitem">
+              <span class="menu-item">
               <a class="d-flex flex-column align-items-center wes-font-icon-menu"  data-toggle="collapse" href="#primaryNav" role="button" aria-expanded="false" aria-controls="primaryNav">Menu</a>
               </span>
             </div>
             <!-- end mobile to large masthead //-->
 
             <!-- start extra-large masthead //-->
-            <div class="d-none d-xl-flex flex-wrap" role="group">
-              <span class="menu-item" role="listitem">
+            <div class="d-none d-xl-flex flex-wrap">
+              <span class="menu-item">
                 <a class="d-flex flex-row align-items-center wes-font-icon-user" href="#">Book an appointment</a>
               </span>
-              <span class="menu-item" role="listitem">
+              <span class="menu-item">
                 <a class="d-flex flex-row align-items-center wes-font-icon-user" href="#">Contact us</a>
               </span>
-              <span class="menu-item" role="listitem">
+              <span class="menu-item">
                 <a class="d-flex flex-row align-items-center wes-font-icon-chat" href="#">Live chat</a>
               </span>
-              <span class="menu-item" role="listitem">
+              <span class="menu-item">
                 <a class="d-flex flex-row align-items-center wes-font-icon-search" data-toggle="collapse" href="#searchPanel" role="button" aria-expanded="false" aria-controls="searchPanel">Search</a>
               </span>
             </div>
@@ -118,7 +123,7 @@ export const header = () => ({
     <!-- end primaryNav //-->
 
       <!-- start searchPanel //-->
-      <div class="collapse search-panel position-relative" id="searchPanel">
+      <div class="collapse search-panel position-relative" id="searchPanel" role="search">
         <div class="underlay position-absolute"></div>
         <div class="position-absolute w-100 wes-bg-solid-white">
           <div class="container pt-2 pb-2">
